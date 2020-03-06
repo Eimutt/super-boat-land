3688d92af9612d5afec2ada43d5b9046ab8ef868
Daniel <dpar@kth.se>
2020-03-06 17:48:45 +0100

    Add boat controller script

---
+++ Super-boat-land/Assets/Scripts/BoatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    private CharacterController controller;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
