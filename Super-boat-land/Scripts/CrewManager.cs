dde75d72003eb7d8394bf8481c7141841f669219
ssiren <ssiren@kth.se>
2020-03-06 19:50:01 +0100

    Add attack movement

---
+++ Super-boat-land/Scripts/CrewManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+	private List<Crew> crewList;
+    void Start()
+    {
+
+		var settings = GameObject.FindObjectsOfType<Settings>();
+        crewList = new List<Crew>();
+        var crews = GameObject.FindObjectsOfType<Crew>();
+        foreach (var crew in crews)
+        {
+			crew.Settings = settings[0];
+            crew.CrewManager = this;
+			crewList.Add(crew);
+		}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Crew crew in crewList)
+        {
+            crew.UpdateCrew(Time.fixedDeltaTime);
+        }
+    }
+
+}
