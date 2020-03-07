cc9b71f4d83c48657c0759af63946ca4ecd52448
Eimutt <emilbojonsson@gmail.com>
2020-03-07 20:29:26 +0100

    Water shader

---
+++ Super-boat-land/Assets/Scripts/WaterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+    private Renderer renderer;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        renderer.material.SetFloat("_GameTime", timer);
+    }
+}
