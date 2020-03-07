d9a99db073fd6b840e25798a45b9ba7f390be935
Eimutt <emilbojonsson@gmail.com>
2020-03-07 15:46:34 +0100

    Add Yarnspinner to project

---
+++ Super-boat-land/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Dialogue : MonoBehaviour
+{
+    public string[] dialogue;
+    public int strIndex;
+    public float textSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextText()
+    {
+
+    }
+
+    public void Interact()
+    {
+
+    }
+}
