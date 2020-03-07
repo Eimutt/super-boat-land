160092678f84827fd5ca407e02868d46b5728afe
Willy Liu <willyjliu@gmail.com>
2020-03-07 13:58:03 +0100

    Undo fuckup (#20)
    * Revert ":)"
    
    This reverts commit 67ef4093cdbc1823ef94f80377a9c5373d95394a.
    
    * Revert "Add items"
    
    This reverts commit 1f501e918ab4d37e81d6e6333faaa32dd2b30bf0.

--- Super-boat-land/Assets/Scripts/Settings.cs
+++ Super-boat-land/Assets/Scripts/Settings.cs
@@ -11,21 +11,17 @@ public class Settings : MonoBehaviour
         get { return m_AttackRange; }
         set { m_AttackRange = value; }
     }
-	public Inventory Inventory { get; set;}
+	
 	public EnemyManager EnemyManager { get; set; }
 	public CrewManager CrewManager { get; set; }
-	public static BoatController BoatController { get; set; }
-	
     // Start is called before the first frame update
     void Start()
     {
-		//Inventory = GameObject.FindObjectsOfType<Inventory>()[0];
-		BoatController = GameObject.FindObjectsOfType<BoatController>()[0];
 		EnemyManager = GameObject.FindObjectsOfType<EnemyManager>()[0];
+        //EnemyManager = GameObject.Find("EnemyManager");
 		CrewManager = GameObject.FindObjectsOfType<CrewManager>()[0];
-		Inventory = GetComponent<Inventory>();
-		
-		
+		Debug.Log(CrewManager);
+		//CrewManager = GameObject.Find("CrewManager");
     }
 
     // Update is called once per frame
