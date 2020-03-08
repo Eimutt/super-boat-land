a28f67016f50d04ca2e8652b1a424062484237c1
ssiren <ssiren@kth.se>
2020-03-08 20:33:31 +0100

    Add spawning crew, killing crew

--- Super-boat-land/Assets/Scripts/CrewManager.cs
+++ Super-boat-land/Assets/Scripts/CrewManager.cs
@@ -7,33 +7,61 @@ public class CrewManager : MonoBehaviour
     // Start is called before the first frame update
 	private List<Crew> crewList;
 	public float SeparationRadius = 0.15f;
+	public List<Crew> realCrew;
+	private LandMovementHandler Captain;
+	private Settings settings;
     void Start()
     {
-		var captain = GameObject.FindObjectsOfType<LandMovementHandler>();
+		Captain = GameObject.FindObjectsOfType<LandMovementHandler>()[0];
 		//var captain = GetComponent<LandMovementHandler>();
-		var settings = GameObject.FindObjectsOfType<Settings>();
+		settings = GameObject.FindObjectsOfType<Settings>()[0];
         crewList = new List<Crew>();
         var crews = GameObject.FindObjectsOfType<Crew>();
+		realCrew = new List<Crew>();
+		
 		//Debug.Log(crewList);
         foreach (var crew in crews)
         {
-			crew.Captain = captain[0];
-			crew.Settings = settings[0];
-            crew.CrewManager = this;
+			
 			crewList.Add(crew);
+			Vector2 randomVector = new Vector2(UnityEngine.Random.Range(-0.5f, 1.0f), UnityEngine.Random.Range(-0.5f, 1.0f));
+			realCrew.Add(Instantiate(crew, randomVector, new Quaternion(0,0,0,0)));
+		}
+		foreach(Crew crew in realCrew){
+			
+			crew.Captain = Captain;
+			crew.Settings = settings;
+            crew.CrewManager = this;
+			
 		}
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        foreach (Crew crew in crewList)
+		realCrew.RemoveAll(item => item == null);
+        foreach (Crew crew in realCrew)
         {
             crew.UpdateCrew(Time.fixedDeltaTime);
         }
+		if (Input.GetKeyDown(KeyCode.J)&& !realCrew[0].Equals(null)){
+			Destroy(realCrew[0].gameObject);
+			realCrew.RemoveAll(item => item == null);
+		}
+		if (Input.GetKeyDown(KeyCode.L) && realCrew.Count < 5){
+			Vector2 randomVector = new Vector2(UnityEngine.Random.Range(-0.5f, 1.0f), UnityEngine.Random.Range(-0.5f, 1.0f));
+			realCrew.Add(Instantiate(crewList[0], randomVector, new Quaternion(0,0,0,0)));
+			foreach(Crew crew in realCrew){
+			
+			crew.Captain = Captain;
+			crew.Settings = settings;
+            crew.CrewManager = this;
+			}
+		}
+            
     }
 	public List<Crew> getCrew(){
-		return crewList;
+		return realCrew;
 	}
 
 }
