24359383b380809f36237c7c07361dd1894e6eb4
Daniel <dpar@kth.se>
2020-03-09 00:15:42 +0100

    Circle timer for Enemy AOE atk

--- Super-boat-land/Assets/AI_Scripts/ChargeBehaviour.cs
+++ Super-boat-land/Assets/AI_Scripts/ChargeBehaviour.cs
@@ -6,10 +6,15 @@ public class ChargeBehaviour : StateMachineBehaviour
 {
     public float chargeDuration;
     private float ogDuration;
+    public GameObject ChargeRing; //The ring that visualizes frog attack.
+    private GameObject chargeRingCopy;
+    private bool charge;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         ogDuration = chargeDuration;
+        charge = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -17,9 +22,24 @@ public class ChargeBehaviour : StateMachineBehaviour
     {
         chargeDuration -= Time.deltaTime;
         Debug.Log(chargeDuration);
-        //Charges the attack for a few seconds before firing off.
-        if(chargeDuration < 0.0f)
+        if(chargeDuration/ogDuration<0.90f && !charge)
         {
+
+            chargeRingCopy = Instantiate(ChargeRing, animator.transform.position, Quaternion.identity);
+            charge = true;
+
+        }
+        if(charge)
+        {
+            //SET THE TIME VARIABLE INSIDE THE SHADER TO MAKE IT ANIMATE.
+            Renderer rend = chargeRingCopy.GetComponent<Renderer>();
+            float fraction = Mathf.Min(1.0f, 1-(chargeDuration / ogDuration));
+            rend.material.SetFloat("_TimeFraction", fraction);
+        }
+        //Charges the attack for a few seconds before firing off.
+        if (chargeDuration < 0.0f)
+        {
+            chargeRingCopy.SetActive(false);
             animator.transform.GetComponent<Attack>().AOEAttack();
             animator.SetBool("isCharging", false);
             chargeDuration = int.MaxValue;
@@ -31,6 +51,7 @@ public class ChargeBehaviour : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         chargeDuration = ogDuration;
+        Destroy(chargeRingCopy);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
