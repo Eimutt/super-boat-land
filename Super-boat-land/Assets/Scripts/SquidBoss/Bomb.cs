b9418a5ac061d274934c95a6fb4daf5e75a0a851
Eimutt <emilbojonsson@gmail.com>
2020-03-08 20:10:16 +0100

    Squid boss (#38)
    * Add boss to map
    
    * Add new boss scene
    
    * Add hookable bomb
    
    * Move files around

---
+++ Super-boat-land/Assets/Scripts/SquidBoss/Bomb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private bool hooked;
+    public float hookSpeed;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hooked)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, hookSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Hook()
+    {
+        print("Bomb caught");
+        hooked = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void UnHook()
+    {
+        print("Bomb released");
+        hooked = false;
+    }
+
+    public void Explode()
+    {
+
+    }
+}
