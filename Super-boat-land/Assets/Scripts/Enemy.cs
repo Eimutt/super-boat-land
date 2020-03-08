adbaeaf0c2842f7a756a1b5f94bf8526221a2ee2
Willy Liu <willyjliu@gmail.com>
2020-03-08 18:22:36 +0100

    Enemy healthbar (#36)
    * Add hp-bar sprite
    
    * Change pivot of empty-hp-bar
    
    * Add healthbar game object
    
    * Add health bar to enemies
    
    * Copy ZlotnikScene to WillyScene

--- Super-boat-land/Assets/Scripts/Enemy.cs
+++ Super-boat-land/Assets/Scripts/Enemy.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-	private Vector2 startPosition; //This should never change
+    private Vector2 startPosition; //This should never change
     public Vector2 Position; //This should never change
     public EnemyManager EnemyManager { get; set; }
     public Settings Settings { get; set; }
-	private int health;
-	public static bool isAlive = true;
+    private int health;
+    public int maxHealth = 1000;
+    public static bool isAlive = true;
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-		//int health;
         startPosition = gameObject.transform.position;
-		health = 1000;
-		Settings = FindObjectsOfType<Settings>()[0];
-		EnemyManager = FindObjectsOfType<EnemyManager>()[0];
-		Position = transform.position;
+        health = maxHealth;
+        Settings = FindObjectsOfType<Settings>() [0];
+        EnemyManager = FindObjectsOfType<EnemyManager>() [0];
+        Position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(this);
+
     }
-	public void UpdateEnemy(float deltaTime){
-		Position = transform.position;
-	}
+
+    public void UpdateEnemy(float deltaTime)
+    {
+        Position = transform.position;
+    }
 
     /*
      *  This is used for the AI to generate a random position.
@@ -37,27 +41,36 @@ public class Enemy : MonoBehaviour
     {
         return startPosition;
     }
-	public bool damage(int damage){
-		setHealth(getHealth() - damage);
-		//health -=damage;
-		Debug.Log(getHealth());
-		if (getHealth() <=0){
-			isAlive = false;
-			Settings.Captain.enemyKilled(this.gameObject);
-			
-			//Destroy(this.gameObject);
-			return true;
-		}
-		return false;
-	}
-	private void setHealth (int newHealth){
-		health = newHealth;
-		
-	}
-	public int getHealth (){
-		return health;
-	}
-	public bool getIsAlive(){
-		return isAlive;
-	}
+
+    public bool damage(int damage)
+    {
+        setHealth(getHealth() - damage);
+        //health -=damage;
+        Debug.Log(getHealth());
+        if (getHealth() <= 0)
+        {
+            isAlive = false;
+            Settings.Captain.enemyKilled(this.gameObject);
+
+            //Destroy(this.gameObject);
+            return true;
+        }
+        return false;
+    }
+
+    private void setHealth(int newHealth)
+    {
+        health = newHealth;
+        healthBar.SetHealth(health / maxHealth);
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public bool getIsAlive()
+    {
+        return isAlive;
+    }
 }
