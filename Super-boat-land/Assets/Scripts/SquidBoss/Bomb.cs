0f89295fc8bc4e4887d279dbacf031bda6ccbbf2
Simon Edström Kawaji <kawaji@kth.se>
2020-03-09 17:17:27 +0100

    Tried to add explosion sound

--- Super-boat-land/Assets/Scripts/SquidBoss/Bomb.cs
+++ Super-boat-land/Assets/Scripts/SquidBoss/Bomb.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
-{
+public class Bomb : MonoBehaviour {
     private bool hooked;
     public float hookSpeed;
     private GameObject player;
@@ -14,57 +13,55 @@ public class Bomb : MonoBehaviour
     private Vector3 p2;
     public float arcDuration;
     private float tsum;
-    void Start()
-    {
+    AudioSource audioSource;
+    public AudioClip explosionSound;
 
+
+
+    void Start() {
+        audioSource = GameObject.FindGameObjectWithTag("AudioSource").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (moving)
-        {
+    void Update() {
+        if (moving) {
             tsum += Time.deltaTime;
-            gameObject.transform.position = Bezier(tsum/arcDuration);
-            if(tsum/arcDuration > 1)
-            {
+            gameObject.transform.position = Bezier(tsum / arcDuration);
+            if (tsum / arcDuration > 1) {
                 moving = false;
                 gameObject.GetComponent<BoxCollider>().enabled = true;
             }
         }
-        if (hooked)
-        {
+        if (hooked) {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, hookSpeed * Time.deltaTime);
         }
     }
 
-    public void Hook()
-    {
+    public void Hook() {
         hooked = true;
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    public void UnHook()
-    {
+    public void UnHook() {
         hooked = false;
     }
 
-    public void Explode()
-    {
+    public void Explode() {
         Instantiate(explosionEffect, gameObject.transform.position, Quaternion.identity);
+        if (audioSource != null) {
+            //audioSource.PlayOneShot(explosionSound);
+        }
         Destroy(gameObject);
     }
 
-    public void SetCurve(Vector3 start, Vector3 mid, Vector3 end)
-    {
+    public void SetCurve(Vector3 start, Vector3 mid, Vector3 end) {
         p0 = start;
         p1 = mid;
         p2 = end;
         moving = true;
     }
 
-    public Vector3 Bezier(float t)
-    {
+    public Vector3 Bezier(float t) {
         Vector3 position = (Mathf.Pow(1 - t, 2) * p0 + 2 * t * (1 - t) * p1 + Mathf.Pow(t, 2) * p2);
         return position;
     }
