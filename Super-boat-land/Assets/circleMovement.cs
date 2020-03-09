3bfb798b66efeb3eda4cf1821e9ccde7cebd4165
Daniel <dpar@kth.se>
2020-03-09 19:04:00 +0100

    grassLand done sort of

---
+++ Super-boat-land/Assets/circleMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleMovement : MonoBehaviour
+{
+    float speed;
+    float turnPoint;
+    float distanceTraveled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.5f;
+        turnPoint = 1.0f;
+        distanceTraveled = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<CharacterController>().Move(new Vector2(speed * Time.deltaTime, 0.0f));
+        distanceTraveled += speed * Time.deltaTime;
+        if (Mathf.Abs(distanceTraveled) > turnPoint)
+        {
+            speed *= -1;
+            distanceTraveled = 0;
+        }
+    }
+}
---
+++ Super-boat-land/Assets/flowerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flowerController : MonoBehaviour
+{
+    BoxCollider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = transform.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Player")
+        {
+            SceneManager.LoadScene("GrassLand");
+            Debug.Log("SADSD");
+        }
+    }
+}
